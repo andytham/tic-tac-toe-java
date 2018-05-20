31a535db6a3eb3e7b943947ba6724fd1ba74ff2b
andy <thamandyy@gmail.com>
2018-05-20 16:46:26 -0400

    added start menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,22 +12,29 @@ public class Main {
 
         Board board = new Board();
         System.out.println("TIC TAC TOE");
-        System.out.println("Select an integer to place\n");
-        System.out.println("1|2|3");
-        System.out.println("-|-|-");
-        System.out.println("4|5|6");
-        System.out.println("-|-|-");
-        System.out.println("7|8|9\n");
-
-
+//        System.out.println("Select an integer to place\n");
+//        System.out.println("1|2|3");
+//        System.out.println("-|-|-");
+//        System.out.println("4|5|6");
+//        System.out.println("-|-|-");
+//        System.out.println("7|8|9\n");
         Scanner reader = new Scanner(System.in);
-        boolean gameRunning = true;
-        String currentPlayer = "X";
-//        TwoPlayers two = new TwoPlayers();
-//        two.runGame();
+        System.out.println("Choose a game mode:\n 1. Two Player\n 2. Versus Computer");
 
-        AIGame ai = new AIGame();
-        ai.runGame();
+        String input = reader.nextLine();
+        boolean inputCheck = false;
+        do {
+                if (input.equals("1") || input.equals("2")){
+                    inputCheck = true;
+                    startGame(input);
+                } else {
+                    input = reader.nextLine();
+                }
+
+        } while (!inputCheck);
+
+
+
 //        while(gameRunning){
 //            System.out.println(currentPlayer + "'s turn: ");
 //            placePiece(reader, currentPlayer, board);
@@ -74,5 +81,17 @@ public class Main {
         }
         while(!success);
 
+    }
+
+    public static void startGame(String input){
+        if (input.equals("1")){
+            TwoPlayers two = new TwoPlayers();
+            two.runGame();
+        } else if (input.equals("2")){
+            AIGame ai = new AIGame();
+            ai.runGame();
+        } else {
+
+        }
     }
 }
